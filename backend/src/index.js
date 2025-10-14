8e868be3610d03073b9f14567681d18137116574
imtarget <binhtan5734@gmail.com>
2025-10-14 22:23:06 +0700

    Thiet ke endpoint signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,10 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
+
+
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log("Server is running on port:" + PORT);
